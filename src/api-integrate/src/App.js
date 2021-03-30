bab5876ef9c86c0e9f230e457abac7325f556d13
leejunsu <qkfhtit@gmail.com>
2021-03-30 17:38:09 +0900

    20210330

--- src/api-integrate/src/App.js
+++ src/api-integrate/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Users_default from "./Users_default";
+import Users from "./Users";
 
 const App = () => {
     return (
         <div>
-          <Users_default/>
+          <Users/>
         </div>
     );
 };
